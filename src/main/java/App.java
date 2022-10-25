631b456a51abd4779a9ba670962acaa2e29f61cf
waynemorphic <wayneklyde@gmail.com>
2022-10-25 21:54:41 +0300

    refactored App.java and its consequent test cases

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,65 +1,47 @@
+/*
+* - develop a method that prints from 1 to a number entered by a user
+* - number should only be positive
+* - if number entered by user is negative, return 1
+*
+* */
+
 import java.io.*;
 import java.util.ArrayList;
 
 public class App {
-    public static void main(String[] args){
-        System.out.println("hello there");
-        System.out.println(addition(4,4));
-
-        App.replaceNums();
-    }
-    static int addition(int numA, int numB){
-        int numSum = numA + numB;
-        return numSum;
-    }
-
-    /*
-    * - create a method that takes a number from a user and returns a range of
-    *   numbers from 1 to the chosen number with the following exceptions
-    *
-    * 1. Numbers divisible by 3 are replaced with the String "ping"
-    * 2. Numbers divisible by 5 are replaced with the String "pong"
-    * 3. Numbers divisible by 15 are replaced with a String "ping-pong"*/
-
-    static ArrayList<Object> replaceNums() {
-        int constNum = 1;
-        int nums;
-        ArrayList<Object> arrayList = new ArrayList<Object>();
-
-        System.out.println("Enter a number");
+    public static void main(String[] args) {
         try {
+            System.out.println("Enter a number");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            String entryNum = bufferedReader.readLine();
-            nums = Integer.parseInt(entryNum);
+            String resNum = bufferedReader.readLine();
+            int parseRes = Integer.parseInt(resNum);
 
-            System.out.println("The range of numbers from 1 to " + " " + nums);
-
-
-            for (int i = 1; i < nums; i++) {
-//                arrayList.add(i);
-                System.out.println(arrayList);
-////                String resArray = arrayList.toString();
-//                return arrayList.toString();
-
-                if (i % 3 == 0) {
-                    arrayList.add("Ping");
-
-                } else if (i % 5 == 0) {
-                    arrayList.add("Pong");
-
-                } else if (i % 3 == 0 && nums % 5 == 0) {
-                    arrayList.add("Ping Pong");
-                }
-                else{
-                    arrayList.add(i);
-                }
-                System.out.println(arrayList);
-                return arrayList;
-            }
-
+            System.out.println(App.replaceNums(parseRes));
         } catch (IOException e) {
             e.printStackTrace();
+
+        }
+
+        System.out.println(addition(2, 3));
+    }
+
+    public static int addition(int numX, int numY) {
+        return (numX + numY);
+    }
+
+
+    static ArrayList<Object> replaceNums(int nums) {
+        ArrayList<Object> arrayList = new ArrayList<Object>();
+
+        int n = 1;
+        if (nums < n){
+            arrayList.add(1);
+        }
+
+        for (int i = 1; i <= nums; i++) {
+            arrayList.add(i);
         }
         return arrayList;
     }
 }
+
--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 
+/*
+ * - create a method that takes a number from a user and returns a range of
+ *   numbers from 1 to the chosen number with the following exceptions
+ *
+ * 1. Numbers divisible by 3 are replaced with the String "ping"
+ * 2. Numbers divisible by 5 are replaced with the String "pong"
+ * 3. Numbers divisible by 15 are replaced with a String "ping-pong"*/
+
 public class PingPong {
     public static void main(String[] args){
         System.out.println(PingPong.changeNums(15));
